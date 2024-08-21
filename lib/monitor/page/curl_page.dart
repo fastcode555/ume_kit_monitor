bc6dbb102403b901263de668c7e821e03e8e9144
Barry <87644022+fastcode555@users.noreply.github.com>
2024-08-21 11:41:08 +0700

    feat：Add a number to the array or model to indicate the number of models or arrays

--- lib/monitor/page/curl_page.dart
+++ lib/monitor/page/curl_page.dart
@@ -26,7 +26,7 @@ class _CurlPageState extends State<CurlPage> {
 
   ///更新正则,已匹配转义后的链接
   static RegExp _regexUrl =
-  RegExp(r"(https?|ftp|file):(//|\\/\\/)[-A-Za-z0-9+&@#/\%?\\/=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]"); //匹配url
+      RegExp(r"(https?|ftp|file):(//|\\/\\/)[-A-Za-z0-9+&@#/\%?\\/=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]"); //匹配url
 
   ///都是正则匹配,为此为数据添加临时缓存widget,以进一步提高性能
   static Map<String?, Map<String?, Widget?>> _tabWidgetCached = Map();
@@ -69,6 +69,9 @@ class _CurlPageState extends State<CurlPage> {
     }
   }
 
+  bool get showInputField =>
+      widget.tag == 'Response' || widget.tag == 'Curl' || widget.tag == 'AesDecode' || widget.tag == 'AesDecodes';
+
   @override
   Widget build(BuildContext context) {
     MonitorMessageNotifier<String>? notifier = Monitor.instance.getNotifier(widget.tag);
@@ -79,7 +82,7 @@ class _CurlPageState extends State<CurlPage> {
     return ValueListenableBuilder<List<String>>(
       valueListenable: notifier!.notifier!,
       builder: (_, List<String> datas, child) {
-        if (widget.tag == 'Curl' || widget.tag == 'AesDecode' || widget.tag == 'AesDecodes') {
+        if (showInputField) {
           _startFilter();
           return Column(
             children: [
@@ -92,9 +95,7 @@ class _CurlPageState extends State<CurlPage> {
                     ),
                   ),
                   IconButton(
-                    onPressed: () {
-                      Navigator.of(context).pushNamed(LogRecorderPage.routeName);
-                    },
+                    onPressed: () => Navigator.of(context).pushNamed(LogRecorderPage.routeName),
                     icon: Icon(Icons.view_list_sharp, color: Colors.white),
                   ),
                 ],
@@ -117,41 +118,38 @@ class _CurlPageState extends State<CurlPage> {
       removeTop: true,
       context: context,
       child: ListView.builder(
-        itemBuilder: (_, index) =>
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 8),
-              child: GestureDetector(
-                onTap: () {
-                  String content = index >= results!.length ? "" : results[index];
-                  if (InnerUtils.isEmpty(content)) {
-                    return;
+        itemBuilder: (_, index) => Padding(
+          padding: EdgeInsets.symmetric(vertical: 8),
+          child: GestureDetector(
+            onTap: () {
+              String content = index >= results!.length ? "" : results[index];
+              if (InnerUtils.isEmpty(content)) {
+                return;
+              }
+              if (widget.tag == 'Curl' && content.startsWith("curl -X GET")) {
+                //只有Get 的请求可以直接访问
+                Iterable<RegExpMatch> matchers = _regexUrl.allMatches(content);
+                if (matchers.isNotEmpty) {
+                  String? regexText = matchers.elementAt(0).group(0);
+                  if (!InnerUtils.isEmpty(regexText)) {
+                    InnerUtils.jumpLink(regexText);
                   }
-                  if (widget.tag == 'Curl' && content.startsWith("curl -X GET")) {
-                    //只有Get 的请求可以直接访问
-                    Iterable<RegExpMatch> matchers = _regexUrl.allMatches(content);
-                    if (matchers.isNotEmpty) {
-                      String? regexText = matchers.elementAt(0).group(0);
-                      if (!InnerUtils.isEmpty(regexText)) {
-                        InnerUtils.jumpLink(regexText);
-                      }
-                    }
-                  }
-                },
-                onLongPress: () {
-                  String content = index >= results!.length ? "" : results[index];
-                  //复制页面名字时，做特殊处理
-                  if (widget.tag == 'Page') {
-                    //page页面复制,只复制最后的字段
-                    content = content
-                        .split('/')
-                        .last;
-                  }
-                  Clipboard.setData(ClipboardData(text: content));
-                  showToast('Copied successfully');
-                },
-                child: _buildCachedWidget(index >= results!.length ? "" : results[index]),
-              ),
-            ),
+                }
+              }
+            },
+            onLongPress: () {
+              String content = index >= results!.length ? "" : results[index];
+              //复制页面名字时，做特殊处理
+              if (widget.tag == 'Page') {
+                //page页面复制,只复制最后的字段
+                content = content.split('/').last;
+              }
+              Clipboard.setData(ClipboardData(text: content));
+              showToast('Copied successfully');
+            },
+            child: _buildCachedWidget(index >= results!.length ? "" : results[index]),
+          ),
+        ),
         itemCount: results?.length ?? 0,
       ),
     );
@@ -178,12 +176,12 @@ class _CurlPageState extends State<CurlPage> {
           InnerUtils.isEmpty(_controller.text)
               ? Text(highLightText, style: TextStyle(color: Colors.cyanAccent))
               : _formatColorRichText(
-            highLightText.replaceAll(_controller.text, '[${_controller.text}]'),
-            [
-              TextStyle(color: Colors.cyanAccent),
-              TextStyle(color: Colors.red),
-            ],
-          ),
+                  highLightText.replaceAll(_controller.text, '[${_controller.text}]'),
+                  [
+                    TextStyle(color: Colors.cyanAccent),
+                    TextStyle(color: Colors.red),
+                  ],
+                ),
           JsonShrinkWidget(json: text, deepShrink: 1, indentation: "  "),
         ],
       );
@@ -210,11 +208,12 @@ class _CurlPageState extends State<CurlPage> {
 
   //第一个颜色为文本的默认颜色,其它颜色为为格式化的富文本的颜色,匹配中括号内的东西,中括号内的作为富文本不同颜色的部分,
   // 正则匹配中括号的东西,传入的TextStyle列表,给对应中括号内容,设置不同颜色风格
-  RichText _formatColorRichText(String content,
-      List<TextStyle> styles, {
-        TextAlign textAlign = TextAlign.left,
-        TextOverflow overflow = TextOverflow.visible,
-      }) {
+  RichText _formatColorRichText(
+    String content,
+    List<TextStyle> styles, {
+    TextAlign textAlign = TextAlign.left,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
     List<TextSpan> spans = [];
     Iterable<RegExpMatch> matchers = _regex.allMatches(content);
     //第二个开始才是真正需要格式化的颜色
